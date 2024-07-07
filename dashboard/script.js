f15ada0ae5b14fd1d4d76a293f0bdf473634002d
John Kokar <princedibloni4@gmail.com>
2024-07-06 21:05:35 -0700

    nhh

--- dashboard/script.js
+++ dashboard/script.js
@@ -12,86 +12,7 @@ const FilterRequest = async (coop_id) => {
     </div>
     `;
 
-  const agriculteurb = await requesttoBackend('GET', `BefreeAgriculter/ByIdgetBefreeAgrulter/${coop_id}`);
-
-  if (agriculteurb && agriculteurb.length) {
-    render_agriculter.innerHTML = "";
-    agriculteurb.forEach((agricul) => {
-      const agriculHtml = `
-            <a class="products-row clicbleclass" href="details_view#${agricul.identifiant_interne_exploitation}"  target="_blank">
-                        <button class="cell-more-button">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24"
-                                fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
-                                stroke-linejoin="round" class="feather feather-more-vertical">
-                                <circle cx="12" cy="12" r="1" />
-                                <circle cx="12" cy="5" r="1" />
-                                <circle cx="12" cy="19" r="1" />
-                            </svg>
-                        </button>
-                        <div class="product-cell image">
-                     <img src="${agricul.document ? agricul.document : "dashboard/asserts/avatay.png"}" alt="product">
-                            <span>${agricul.identifiant_interne_exploitation}</span>
-                        </div>
-                        <div class="product-cell category">
-                            <span class="cell-label">Année de naissance:</span>
-                            ${agricul.annee_naissance}
-                        </div>
-                        <div class="product-cell status-cell">
-                            <span class="cell-label">Genre:</span>
-                         <span class="status ${agricul.genre === "HOMME" ? "active" : "disabled"}">${agricul.genre}</span>
-                        </div>
-                        <div class="product-cell sales">
-                            <span class="cell-label">Prénom:</span>
-                            ${agricul.prenom}
-                        </div>
-                        <div class="product-cell stock">
-                            <span class="cell-label">Nom:</span>
-                            ${agricul.nom}
-                        </div>
-                        <div class="product-cell price">
-                            <span class="cell-label">Prenom:</span>
-                            ${agricul.numero_telephone}
-                        </div>
-                    </a>
-
-            `;
-      render_agriculter.innerHTML += agriculHtml;
-    });
-  } else {
-    render_agriculter.innerHTML = `
-     <div style="width: 100%; padding: 10px; border-radius: 10px; background: #ffffff; text-align: center; margin-top: 20px">
-        <div style="width: 100%; height: 250px;">
-          <div style="height: 40%; width: 200px;">
-          </div>
-                <p>Vide (${agriculteurb.length})</p>
-          
-        </div>
-    </div>
-    `;
-  }
-
-}
-
-const ResetFilter = async () => {
-  const categori_listing = document.getElementById("categori_listing");
-  const coop_listing = document.getElementById("coop_listing");
-  categori_listing.innerHTML = "<option>Patientez encours ...</option>";
-  coop_listing.innerHTML = "<option>Patientez encours ...</option>";
-  const ready_tofilter = document.getElementById("ready_tofilter");
-  ready_tofilter.style.display = "none";
-
-  document.querySelector(".filter-menu").classList.toggle("active");
-  const render_agriculter = document.getElementById('render_agriculter');
-  render_agriculter.innerHTML = `
-     <div style="width: 100%; padding: 10px; border-radius: 10px; background: #ffffff; text-align: center; margin-top: 20px">
-        <div style="width: 100%; height: 250px;">
-                <img src="dashboard/loadingc.gif" style="height: 80%; width: 200px;" alt="">
-                <p>En cours ...</p>
-        </div>
-    </div>
-    `;
-
-    const agriculteurb = await requesttoBackend('GET', `BefreeAgriculter/getAllBefreeAgrulter/0/100`);
+  const agriculteurb = await requesttoBackend('GET', `BefreeAgriculter/ByIdgetBefreeAgrulter/${coop_id}/0/100`);
 
   if (agriculteurb.agriculter && agriculteurb.agriculter.length) {
     document.getElementById('agri_lenthg').innerText = `Total: ${agriculteurb.agrilength}`;
@@ -143,7 +64,87 @@ const ResetFilter = async () => {
         <div style="width: 100%; height: 250px;">
           <div style="height: 40%; width: 200px;">
           </div>
-                <p>Vide (${agriculteurb.length})</p>
+                <p>Vide (0)</p>
+          
+        </div>
+    </div>
+    `;
+  }
+
+}
+
+const ResetFilter = async () => {
+  const categori_listing = document.getElementById("categori_listing");
+  const coop_listing = document.getElementById("coop_listing");
+  categori_listing.innerHTML = "<option>Patientez encours ...</option>";
+  coop_listing.innerHTML = "<option>Patientez encours ...</option>";
+  const ready_tofilter = document.getElementById("ready_tofilter");
+  ready_tofilter.style.display = "none";
+
+  document.querySelector(".filter-menu").classList.toggle("active");
+  const render_agriculter = document.getElementById('render_agriculter');
+  render_agriculter.innerHTML = `
+     <div style="width: 100%; padding: 10px; border-radius: 10px; background: #ffffff; text-align: center; margin-top: 20px">
+        <div style="width: 100%; height: 250px;">
+                <img src="dashboard/loadingc.gif" style="height: 80%; width: 200px;" alt="">
+                <p>En cours ...</p>
+        </div>
+    </div>
+    `;
+
+  const agriculteurb = await requesttoBackend('GET', `BefreeAgriculter/getAllBefreeAgrulter/0/100`);
+
+  if (agriculteurb.agriculter && agriculteurb.agriculter.length) {
+    document.getElementById('agri_lenthg').innerText = `Total: ${agriculteurb.agrilength}`;
+    render_agriculter.innerHTML = "";
+    agriculteurb.agriculter.forEach((agricul) => {
+      const agriculHtml = `
+            <a class="products-row clicbleclass" href="details_view#${agricul.identifiant_interne_exploitation}"  target="_blank">
+                        <button class="cell-more-button">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24"
+                                fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                                stroke-linejoin="round" class="feather feather-more-vertical">
+                                <circle cx="12" cy="12" r="1" />
+                                <circle cx="12" cy="5" r="1" />
+                                <circle cx="12" cy="19" r="1" />
+                            </svg>
+                        </button>
+                        <div class="product-cell image">
+                     <img src="${agricul.document ? agricul.document : "dashboard/asserts/avatay.png"}" alt="product">
+                            <span>${agricul.identifiant_interne_exploitation}</span>
+                        </div>
+                        <div class="product-cell category">
+                            <span class="cell-label">Année de naissance:</span>
+                            ${agricul.annee_naissance}
+                        </div>
+                        <div class="product-cell status-cell">
+                            <span class="cell-label">Genre:</span>
+                         <span class="status ${agricul.genre === "HOMME" ? "active" : "disabled"}">${agricul.genre}</span>
+                        </div>
+                        <div class="product-cell sales">
+                            <span class="cell-label">Prénom:</span>
+                            ${agricul.prenom}
+                        </div>
+                        <div class="product-cell stock">
+                            <span class="cell-label">Nom:</span>
+                            ${agricul.nom}
+                        </div>
+                        <div class="product-cell price">
+                            <span class="cell-label">Prenom:</span>
+                            ${agricul.numero_telephone}
+                        </div>
+                    </a>
+
+            `;
+      render_agriculter.innerHTML += agriculHtml;
+    });
+  } else {
+    render_agriculter.innerHTML = `
+     <div style="width: 100%; padding: 10px; border-radius: 10px; background: #ffffff; text-align: center; margin-top: 20px">
+        <div style="width: 100%; height: 250px;">
+          <div style="height: 40%; width: 200px;">
+          </div>
+                <p>Vide (0)</p>
           
         </div>
     </div>
