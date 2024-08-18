e16809a0ad6b8d2d58a499b4de6f6e495d8d744f
John Kokar <princedibloni4@gmail.com>
2024-08-18 10:11:22 -0700

    dfdaa

--- en/dashboard/htmlinjs.js
+++ en/dashboard/htmlinjs.js
@@ -5,7 +5,7 @@ const headers_html = `
                 style="margin-top: 60px; overflow-y: scroll; height: 93.7%; width: 101%; overflow-x: hidden;">
                 <div class="app-content-header">
                     <h1 class="app-content-headerText"
-                        style="background: #20946d4f; padding: 10px; border-radius: 7px;">AGRICULTEURS</h1>
+                        style="background: #20946d4f; padding: 10px; border-radius: 7px;">FARMERS</h1>
                     <button class="mode-switch" title="Switch Theme">
                         <svg class="moon" fill="none" stroke="currentColor" stroke-linecap="round"
                             stroke-linejoin="round" stroke-width="2" width="24" height="24" viewBox="0 0 24 24">
@@ -18,7 +18,7 @@ const headers_html = `
                 </div>
                 <div class="app-content-actions">
                 <div style="display: flex; column-gap: 5px;">
-                    <input class="search-bar" id="gricul-search-bar" placeholder="Recherche GA-B..." type="text">
+                    <input class="search-bar" id="gricul-search-bar" placeholder="Search GA-B..." type="text">
                     <a onclick="close_reload()" class="search-bar-close" style="padding: 2px 8px 2px 8px; border-radius: 10px; color: red; cursor: pointer; background: rgba(121, 5, 5, 0.507);">x</a>
                 </div>
                     <div class="app-content-actions-wrapper">
@@ -34,24 +34,24 @@ const headers_html = `
                             <div class="filter-menu">
                                 <label>Pays</label>
                                 <select id="pays_listing">
-                                   <option>Patientez encours ...</option>
+                                   <option>Patientez Loading ...</option>
                                 </select>
 
                                 <label>Catégorie</label>
                                 <select id="categori_listing">
-                                    <option>Patientez encours ...</option>
+                                    <option>Patientez Loading ...</option>
                                 </select>
 
                                 <label>Coopérative</label>
                                 <select id="coop_listing">
-                                   <option>Patientez encours ...</option>
+                                   <option>Patientez Loading ...</option>
                                 </select>
                                 <div class="filter-menu-buttons" id="ready_tofilter">
                                     <button id="reset_filter" class="filter-button reset" onclick="ResetFilter()">
-                                        Refaire
+                                        Reset
                                     </button>
                                     <button id="applay_filter" class="filter-button apply" onclick="">
-                                        Appliquez
+                                        Apply
                                     </button>
                                 </div>
                             </div>
@@ -77,7 +77,7 @@ const headers_html = `
 
                 <div class="products-area-wrapper tableView">
                     <div class="products-header">
-                        <div class="product-cell image"  title="Identifiant interne unique d'exploitation agricole">
+                        <div class="product-cell image"  title="Unique Internal Farm Identifier">
                             Identifiant I.E
                             <button class="sort-button">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 512 512">
@@ -95,28 +95,28 @@ const headers_html = `
                                 </svg>
                             </button>
                         </div>
-                        <div class="product-cell status-cell">Genre<button class="sort-button">
+                        <div class="product-cell status-cell">Gender<button class="sort-button">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 512 512">
                                     <path fill="currentColor"
                                         d="M496.1 138.3L375.7 17.9c-7.9-7.9-20.6-7.9-28.5 0L226.9 138.3c-7.9 7.9-7.9 20.6 0 28.5 7.9 7.9 20.6 7.9 28.5 0l85.7-85.7v352.8c0 11.3 9.1 20.4 20.4 20.4 11.3 0 20.4-9.1 20.4-20.4V81.1l85.7 85.7c7.9 7.9 20.6 7.9 28.5 0 7.9-7.8 7.9-20.6 0-28.5zM287.1 347.2c-7.9-7.9-20.6-7.9-28.5 0l-85.7 85.7V80.1c0-11.3-9.1-20.4-20.4-20.4-11.3 0-20.4 9.1-20.4 20.4v352.8l-85.7-85.7c-7.9-7.9-20.6-7.9-28.5 0-7.9 7.9-7.9 20.6 0 28.5l120.4 120.4c7.9 7.9 20.6 7.9 28.5 0l120.4-120.4c7.8-7.9 7.8-20.7-.1-28.5z" />
                                 </svg>
                             </button>
                         </div>
-                        <div class="product-cell sales">Prénom<button class="sort-button">
+                        <div class="product-cell sales">First Name<button class="sort-button">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 512 512">
                                     <path fill="currentColor"
                                         d="M496.1 138.3L375.7 17.9c-7.9-7.9-20.6-7.9-28.5 0L226.9 138.3c-7.9 7.9-7.9 20.6 0 28.5 7.9 7.9 20.6 7.9 28.5 0l85.7-85.7v352.8c0 11.3 9.1 20.4 20.4 20.4 11.3 0 20.4-9.1 20.4-20.4V81.1l85.7 85.7c7.9 7.9 20.6 7.9 28.5 0 7.9-7.8 7.9-20.6 0-28.5zM287.1 347.2c-7.9-7.9-20.6-7.9-28.5 0l-85.7 85.7V80.1c0-11.3-9.1-20.4-20.4-20.4-11.3 0-20.4 9.1-20.4 20.4v352.8l-85.7-85.7c-7.9-7.9-20.6-7.9-28.5 0-7.9 7.9-7.9 20.6 0 28.5l120.4 120.4c7.9 7.9 20.6 7.9 28.5 0l120.4-120.4c7.8-7.9 7.8-20.7-.1-28.5z" />
                                 </svg>
                             </button>
                         </div>
-                        <div class="product-cell stock">Nom<button class="sort-button">
+                        <div class="product-cell stock">Last Name<button class="sort-button">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 512 512">
                                     <path fill="currentColor"
                                         d="M496.1 138.3L375.7 17.9c-7.9-7.9-20.6-7.9-28.5 0L226.9 138.3c-7.9 7.9-7.9 20.6 0 28.5 7.9 7.9 20.6 7.9 28.5 0l85.7-85.7v352.8c0 11.3 9.1 20.4 20.4 20.4 11.3 0 20.4-9.1 20.4-20.4V81.1l85.7 85.7c7.9 7.9 20.6 7.9 28.5 0 7.9-7.8 7.9-20.6 0-28.5zM287.1 347.2c-7.9-7.9-20.6-7.9-28.5 0l-85.7 85.7V80.1c0-11.3-9.1-20.4-20.4-20.4-11.3 0-20.4 9.1-20.4 20.4v352.8l-85.7-85.7c-7.9-7.9-20.6-7.9-28.5 0-7.9 7.9-7.9 20.6 0 28.5l120.4 120.4c7.9 7.9 20.6 7.9 28.5 0l120.4-120.4c7.8-7.9 7.8-20.7-.1-28.5z" />
                                 </svg>
                             </button>
                         </div>
-                        <div class="product-cell price">Téléphone
+                        <div class="product-cell price">Phone
                             <button class="sort-button">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 512 512">
                                     <path fill="currentColor"
@@ -147,23 +147,23 @@ const headers_html = `
                                 <span>GA-BIA-001</span>
                             </div>
                             <div class="product-cell category">
-                                <span class="cell-label">Année de naissance:</span>
+                                <span class="cell-label">Year of Birth:</span>
                                 1980
                             </div>
                             <div class="product-cell status-cell">
                                 <span class="cell-label">Genre:</span>
-                                <span class="status active">Homme</span>
+                                <span class="status active">Man</span>
                             </div>
                             <div class="product-cell sales">
-                                <span class="cell-label">Prenom:</span>
+                                <span class="cell-label">First Name:</span>
                                 Ramde Sylvestre
                             </div>
                             <div class="product-cell stock">
-                                <span class="cell-label">Nom:</span>
+                                <span class="cell-label">Last Name:</span>
                                 BONY
                             </div>
                             <div class="product-cell price">
-                                <span class="cell-label">Prenom:</span>
+                                <span class="cell-label">First Name:</span>
                                 0506369696
                             </div>
                         </a>
